bb72a91747d04e74e0861f23f7cc1d9795234560
Yusri Hilmi <yusrihilmi4@gmail.com>
2024-01-15 16:40:49 +0700

    Commit new

--- bionic-project/src/views/HomeView.vue
+++ bionic-project/src/views/HomeView.vue
@@ -5,73 +5,70 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-12 mx-auto flex flex-wrap">
+      <div class="px-4 py-2 mx-auto flex flex-wrap bg-black">
         <div class=" lg:w-full">
-          <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/1920x600">
+          <img class="object-cover object-center rounded md:h-fit h-[50px]" alt="hero" src="https://dummyimage.com/1920x100/0808f2/0808f2">
         </div>
       </div>
     </section>
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-4 mx-auto flex flex-wrap">
-        <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">Kickstarter Actually Pinterest
+      <div class="px-4 py-2 mx-auto flex flex-wrap bg-black">
+        <h2 class="sm:text-3xl text-m text-gray-900 font-medium title-font border-solid border-black md:w-1/2 text-[#0808f2] bg-[#0808f2] border-4">Kickstarter Actually Pinterest
           Brunch Bitters Occupy</h2>
-        <div class="md:w-3/5 md:pl-6">
+        <div class="md:w-1/2 md:pl-2 border-4 text-[#0808f2] bg-[#0808f2] border-solid border-black">
           <p class="leading-relaxed text-base">Taxidermy bushwick celiac master cleanse microdosing seitan. Fashion axe
-            four dollar toast truffaut, direct trade kombucha brunch williamsburg keffiyeh gastropub tousled squid meh
-            taiyaki drinking vinegar tacos.</p>
+            </p>
         </div>
       </div>
     </section>
-    <section class="text-gray-600 body-font">
-      <div class="container mx-auto flex px-5 py-4 md:flex-row flex-col items-center ">
-        <div class=" lg:w-full md:w-3/5 w-full mb-10 md:mb-0">
-          <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/900x300">
-          <div class="flex flex-wrap md:-m-2 -m-1">
-            <div class="flex flex-wrap w-full pt-4">
-              <div class="md:p-2 p-1 w-1/3">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 ">Master Cleanse
+    <section class="text-gray-600 body-font bg-black">
+      <div class=" mx-auto flex px-5 py-4 md:flex-row flex-col items-start ">
+        <div class=" lg:w-full md:w-3/5 w-full mb-10 md:mb-0 p-5 bg-[#0808f2] ">
+          <img class="object-cover object-center rounded border-solid border-8 border-black" alt="hero" src="https://dummyimage.com/900x300/f04709/f04709">
+          <div class="flex flex-wrap border-solid border-8 border-black mt-4">
+            <div class="flex flex-wrap w-full">
+              <div class="md:p-2 p-1 w-1/3 border-solid border-8 border-black">
+                <h1 class="sm:text-3xl text-m font-medium title-font text-[#0808f2]">Master Cleanse
                   Reliac Heirloom</h1>
-                <p class="leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon
-                  brooklyn
-                  asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man
-                  bun deep
-                  jianbing selfies heirloom.</p>
               </div>
-              <div class="md:p-2 p-1 w-1/3">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 ">Master Cleanse
+              <div class="md:p-2 p-1 w-1/3 border-solid border-8 border-black">
+                <h1 class="sm:text-3xl text-m font-medium title-font text-[#0808f2] ">Master Cleanse
                   Reliac Heirloom</h1>
-                <p class="leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon
-                  brooklyn
-                  asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man
-                  bun deep
-                  jianbing selfies heirloom.</p>
               </div>
-              <div class="md:p-2 p-1 w-1/3">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 ">Master Cleanse
+              <div class="md:p-2 p-1 w-1/3 border-solid border-8 border-black">
+                <h1 class="sm:text-3xl text-m font-medium title-font text-[#0808f2] ">Master Cleanse
                   Reliac Heirloom</h1>
-                <p class="leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon
-                  brooklyn
-                  asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man
-                  bun deep
-                  jianbing selfies heirloom.</p>
               </div>
             </div>
           </div>
         </div>
         <div
-          class="lg:flex-grow md:w-2/5 md:pl-16 flex md:flex-col flex-row md:items-start md:text-left items-center text-center pt-4">
-          <div class="md:p-2 p-1 md:w-full w-1/3">
-            <img alt="gallery" class="w-full object-cover h-full object-center block"
-              src="https://dummyimage.com/501x301">
+          class="lg:flex-grow md:w-2/5 md:pl-4 flex md:flex-col flex-row md:items-start md:text-left items-center text-center">
+          <div class="md:p-2 p-1 md:w-full w-1/3 bg-[#0808f2]">
+            <img alt="gallery" class="w-full object-cover h-full object-center block border-solid border-8 border-black"
+              src="https://dummyimage.com/501x200/f04709/f04709">
           </div>
-          <div class="md:p-2 p-1 md:w-full w-1/3">
-            <img alt="gallery" class="w-full object-cover h-full object-center block"
-              src="https://dummyimage.com/501x301">
+          <div class="md:p-2 p-1 md:w-full w-1/3 bg-[#0808f2]">
+            <img alt="gallery" class="w-full object-cover h-full object-center block border-solid border-8 border-black"
+              src="https://dummyimage.com/501x200/f04709/f04709">
           </div>
-          <div class="md:p-2 p-1 md:w-full w-1/3">
-            <img alt="gallery" class="w-full object-cover h-full object-center block"
-              src="https://dummyimage.com/501x301">
+          <div class="md:p-2 p-1 md:w-full w-1/3 bg-[#0808f2]">
+            <img alt="gallery" class="w-full object-cover h-full object-center block border-solid border-8 border-black"
+              src="https://dummyimage.com/501x200/f04709/f04709">
           </div>
+        </div>
+    </div>
+  </section>
+  <section>
+    <div class="md:block hidden mt-8">
+      <div class=" lg:w-full md:w-2/5 w-full mb-10 md:mb-0 p-5 relative">
+          <img class="object-cover object-center rounded border-solid border-8 border-black" alt="hero" src="https://dummyimage.com/800x300/0808f2/0808f2">
+          <img class="object-cover object-center absolute top-[28px] left-[28px]" alt="hero" src="https://dummyimage.com/200x100/f04709/f04709">
+          <img class="object-cover object-center absolute top-[85px] left-[100px]" alt="hero" src="https://dummyimage.com/185x170/ffffff/ffffff">
+          <img class="object-cover object-center absolute bottom-[28px] left-[170px]" alt="hero" src="https://dummyimage.com/200x100/f04709/f04709">
+          <img class="object-cover object-center absolute top-[28px] right-[425px]" alt="hero" src="https://dummyimage.com/200x100/f04709/f04709">
+          <img class="object-cover object-center absolute bottom-[28px] right-[200px]" alt="hero" src="https://dummyimage.com/200x100/f04709/f04709">
+          
         </div>
     </div>
   </section>
